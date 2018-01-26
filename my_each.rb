80e7dc01e5a0b1769e3c20a4301afe93a89d1252
Veronica Kim <vero.a.kim@gmail.com>
2018-01-26 22:39:48 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,16 @@
 def my_each(array)
-
+  if block_given?
   i = 0
 
   while i < array.length
     yield(array[i])
       i +=1
+    end
+    array
+  else 
+    puts "Hello, you."
 end
-  array
 end
-
 my_each(["james", "jim", "john"]) do |name|
   puts "hello #{name}"
 end
